a2bf64852b8685bc6a926772e88f63e792e0ea00
Kevin Suen <kevinsuen11@gmail.com>
2019-09-29 18:52:43 -0400

    Add reducer and redirect after sending image

--- src/pages/main/snap/snap.component.jsx
+++ src/pages/main/snap/snap.component.jsx
@@ -1,8 +1,15 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+import { Redirect } from 'react-router-dom';
 import Camera, { IMAGE_TYPES } from 'react-html5-camera-photo';
 import 'react-html5-camera-photo/build/css/index.css';
 
 import Axios from 'axios';
+
+import reducer, {
+  SET_PHOTO,
+  SET_REDIRECT_BOOLEAN,
+  SET_ROOM_ID
+} from '../../../reducers/snap';
 
 import useVisualMode from '../../../hooks/useVisualMode';
 
@@ -11,16 +18,21 @@ const SnapPage = () => {
   const PREVIEW = 'PREVIEW';
   const LOADING = 'LOADING';
 
-  const [photo, setPhoto] = useState(null);
+  const [state, dispatch] = useReducer(reducer, {
+    photo: null,
+    redirect: false,
+    roomId: null
+  });
+
   const { mode, transition } = useVisualMode(CAMERA);
 
   const onTakePhoto = dataUri => {
-    setPhoto(dataUri);
+    dispatch({ type: SET_PHOTO, value: dataUri });
     transition(PREVIEW);
   };
 
   const sendImage = () => {
-    const base64Image = photo.split(',');
+    const base64Image = state.photo.split(',');
     const data = new FormData();
     data.append('image_data', base64Image[1]);
 
@@ -28,13 +40,17 @@ const SnapPage = () => {
 
     Axios({
       method: 'post',
-      url: '',
+      url: 'http://6e87abab.ngrok.io/snap',
       data: data,
       config: { headers: { 'Content-Type': 'multipart/form-data' } }
     })
       .then(resp => {
         console.log('success');
         console.log(resp.data);
+        if (resp.data.type === "REDIRECT") {
+          dispatch({ type: SET_ROOM_ID, value: resp.data.payload });
+          dispatch({ type: SET_REDIRECT_BOOLEAN, value: true });
+        }
       })
       .catch(resp => {
         console.log(resp.data);
@@ -44,6 +60,7 @@ const SnapPage = () => {
   return (
     <div>
       <h1>Snap Page</h1>
+
       {mode === CAMERA && (
         <Camera
           onTakePhoto={dataUri => {
@@ -54,14 +71,18 @@ const SnapPage = () => {
           imageType={IMAGE_TYPES.JPG}
         />
       )}
+
       {mode === PREVIEW && (
         <div>
-          <img src={photo} alt="Preview" />
+          <img src={state.photo} alt="Preview" />
           <button onClick={() => transition(CAMERA)}>Take Another</button>
           <button onClick={() => sendImage()}>Send</button>
         </div>
       )}
+
       {mode === LOADING && <h2>DOING SOME MAGIC...</h2>}
+
+      {state.redirect && <Redirect to={`/roominvitation/${state.roomId}`} />}
     </div>
   );
 };

c0d2941d52cbcb9e9adab55d9a9302f7f62cfb87
Kevin Suen <kevinsuen11@gmail.com>
2019-09-30 18:00:42 -0400

    Add initial socket io

--- src/pages/room/room.component.jsx
+++ src/pages/room/room.component.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import ItemList from '../../components/ItemList/item-list.component';
 
 import Axios from 'axios';
+import io from 'socket.io-client';
 
 import roomReducer, {
   SET_BILL_DATA,
@@ -13,6 +14,8 @@ import roomReducer, {
 import { isNull } from 'util';
 
 const RoomPage = () => {
+  const socket = io.connect(process.env.REACT_APP_API_SERVER_URL);
+
   const [state, dispatch] = useReducer(roomReducer, {
     billData: null,
     cartData: [
@@ -27,7 +30,7 @@ const RoomPage = () => {
   const roomId = useParams().id;
 
   useEffect(() => {
-    Axios.get(`http://fcc75922.ngrok.io/room/${roomId}`)
+    Axios.get(`${process.env.REACT_APP_API_SERVER_URL}/room/${roomId}`)
       .then(resp => {
         dispatch({ type: SET_BILL_DATA, value: resp.data });
       })
@@ -37,25 +40,48 @@ const RoomPage = () => {
   }, [roomId]);
 
   const handleSwipe = id => {
-    let bill = {...state.billData[id], is_checked: true};
+    let bill = { ...state.billData[id], is_checked: true };
 
     if (state.billData[id].is_checked) {
-      bill = {...state.billData[id], is_checked: false}
+      bill = { ...state.billData[id], is_checked: false };
     }
+
+    // TODO: FIX EMIT AND ADD EMIT FOR UNCHECK
+    socket.emit('check', 'yolo');
+    console.log('success');
 
     const billData = {
       ...state.billData,
       [id]: bill
-    }
+    };
 
-    dispatch({ type: SET_SELECTED, value: billData })
+    dispatch({ type: SET_SELECTED, value: billData });
   };
+
+  // SOCKET IO
+  useEffect(() => {
+    // EVENT LISTENERS
+    socket.on('connect', () => {
+      console.log(socket.connected);
+      socket.emit('check', 'Ive connected');
+    });
+
+    // TODO: HANDLE SERVER RESPONSE
+    socket.on('check', payload => {
+      console.log(payload);
+    });
+
+    socket.on('uncheck', payload => {
+      console.log(payload);
+    });
+  }, []);
 
   return (
     <div>
       <h1>Room</h1>
+      <button onClick={() => handleSwipe(2)}>EMIT</button>
       {!isNull(state.billData) ? (
-        <ItemList itemsData={state.billData} handleSwipe={handleSwipe}/>
+        <ItemList itemsData={state.billData} handleSwipe={handleSwipe} />
       ) : (
         <h1>LOADING</h1>
       )}

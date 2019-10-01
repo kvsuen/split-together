e06c5b6a64d138abfeb6a5ab7bcdc3a4a70a4a44
jacksonf21 <jacksonf21@hotmail.com>
2019-10-01 16:05:25 -0400

    Add: item swipe for logic dispatch

--- src/components/ItemList/Item/item.component.jsx
+++ src/components/ItemList/Item/item.component.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 
+import { useGesture } from 'react-with-gesture';
+import { useSpring, animated } from 'react-spring';
+
 const Item = ({ id, is_checked, name, unit_price, handleSwipe }) => {
+
+  const [{ xy }, set] = useSpring(() => ({ xy: [0, 0] }))
+  const bind = useGesture(({ down, delta, velocity }) => set({
+    xy: down ? 
+    (delta[0] > 0 ? delta : 
+      gestureLogic(delta, velocity)) : 
+      gestureLogic(delta, velocity)
+    }))
+
+  const gestureLogic = (delta, velocity) => {
+    if (delta[0] > 0 && velocity > 0.5) {
+      document.getElementById(`item${id}`).click()
+    } 
+    return [0, 0];
+  }
+
   return (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
+    <animated.div id={`item${id}`} {...bind()} style={{display: 'flex', alignItems: 'center', transform: xy.interpolate((x, y) => `translate3d(${x}px,0,0)`)}} onClick={() => handleSwipe(id)}>
       <h4>
         name: {name}, price: {unit_price}
       </h4>
@@ -11,7 +30,7 @@ const Item = ({ id, is_checked, name, unit_price, handleSwipe }) => {
         checked={is_checked}
         onClick={() => handleSwipe(id)}
       ></input>
-    </div>
+    </animated.div>
   );
 };
 
--- src/pages/room/room.component.jsx
+++ src/pages/room/room.component.jsx
@@ -15,25 +15,44 @@ import roomReducer, {
 import { isNull } from 'util';
 
 const RoomPage = () => {
-  
   const [state, dispatch] = useReducer(roomReducer, {
-    billData: null,
+    billData: {
+      "1": {
+        "id": 1, 
+        "is_checked": false, 
+        "name": "fries", 
+        "unit_price": 1.5
+      }, 
+      "2": {
+        "id": 2, 
+        "is_checked": false, 
+        "name": "junior chicken", 
+        "unit_price": 2.2
+      },
+      "3": {
+        "id": 3, 
+        "is_checked": false, 
+        "name": "hamburger", 
+        "unit_price": 3.2
+      }
+    },
     cartData: []
   });
-  
+
   const roomId = useParams().id;
   
-  useEffect(() => {
-    Axios.get(`${process.env.REACT_APP_API_SERVER_URL}/room/${roomId}`)
-    .then(resp => {
-      dispatch({ type: SET_BILL_DATA, value: resp.data });
-      })
-      .catch(resp => {
-        console.log(resp);
-      });
-    }, [roomId]);
+  // useEffect(() => {
+  //   Axios.get(`${process.env.REACT_APP_API_SERVER_URL}/room/${roomId}`)
+  //   .then(resp => {
+  //     dispatch({ type: SET_BILL_DATA, value: resp.data });
+  //     })
+  //     .catch(resp => {
+  //       console.log(resp);
+  //     });
+  //   }, [roomId]);
     
   const handleSwipe = id => {
+
     let bill = { ...state.billData[id], is_checked: true };
     let items = [...state.cartData];
     
@@ -41,54 +60,52 @@ const RoomPage = () => {
       bill = { ...state.billData[id], is_checked: false };
       
       //JACKSON - REMOVES ITEM
-      console.log("REMOVE")
       items = items.filter(item => item !== id)
       dispatch({ type: SET_CART_ITEMS, value: items})
     } else {
       //JACKSON - ADDS ITEM ID TO CART
       items.push(id)
-      console.log(items)
       dispatch({ type: SET_CART_ITEMS, value: items})
 
     }
     
     // TODO: FIX EMIT AND ADD EMIT FOR UNCHECK
     // socket.emit('check', 'yolo');
-    console.log('success');
+    // console.log('success');
     
     const billData = {
       ...state.billData,
       [id]: bill
     };
-    
+
     dispatch({ type: SET_SELECTED, value: billData });
   };
-  
+
   // SOCKET IO
-  useEffect(() => {
-    const socket = io.connect(process.env.REACT_APP_API_SERVER_URL);
-    // EVENT LISTENERS
-    socket.on('connect', () => {
-      console.log(socket.connected);
-      socket.emit('check', 'Ive connected');
-    });
+  // useEffect(() => {
+  //   const socket = io.connect(process.env.REACT_APP_API_SERVER_URL);
+  //   // EVENT LISTENERS
+  //   socket.on('connect', () => {
+  //     console.log(socket.connected);
+  //     socket.emit('check', 'Ive connected');
+  //   });
     
-    // TODO: HANDLE SERVER RESPONSE
-    socket.on('check', payload => {
-      console.log(payload);
-    });
+  //   // TODO: HANDLE SERVER RESPONSE
+  //   socket.on('check', payload => {
+  //     console.log(payload);
+  //   });
     
-    socket.on('uncheck', payload => {
-      console.log(payload);
-    });
-  }, []);
+  //   socket.on('uncheck', payload => {
+  //     console.log(payload);
+  //   });
+  // }, []);
 
   return (
     <div>
       <h1>Room</h1>
       <button onClick={() => handleSwipe(2)}>EMIT</button>
       {!isNull(state.billData) ? (
-        <ItemList itemsData={state.billData} handleSwipe={handleSwipe} />
+        <ItemList itemsData={state.billData} handleSwipe={handleSwipe}/>
       ) : (
         <h1>LOADING</h1>
       )}

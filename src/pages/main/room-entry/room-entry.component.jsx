2811a5cb481ee4a755fef6288b5ee18167b2d989
Kevin Suen <kevinsuen11@gmail.com>
2019-10-03 18:58:37 -0400

    Add css styling to room entry page

--- src/pages/main/room-entry/room-entry.component.jsx
+++ src/pages/main/room-entry/room-entry.component.jsx
@@ -2,85 +2,89 @@ import React, { useState } from 'react';
 import Button from '../../../components/Button/button.component';
 import QrReader from 'react-qr-reader';
 import ButtonRedirect from '../../../components/RedirectButton/button-redirect.component';
-import { Redirect } from "react-router-dom"
+import { Redirect } from 'react-router-dom';
 
+import './room-entry.style.css';
 
 const RoomEntryPage = () => {
   const regex = /room\/.*/g;
 
   const [state, setState] = useState({
     qrReaderStatus: false,
-    result: "No result",
-    text: "",
+    result: 'No result',
+    text: '',
     redirect: false
-  })
+  });
 
   const qrScanner = () => {
     if (state.qrReaderStatus) {
-      setState({...state, qrReaderStatus: false})
+      setState({ ...state, qrReaderStatus: false });
     } else {
-      setState({...state, qrReaderStatus: true})
+      setState({ ...state, qrReaderStatus: true });
     }
   };
 
-  function handleScan (data) {
+  function handleScan(data) {
     if (data) {
-      
       const parsedStr = data.match(regex);
-      
+
       setState({
         ...state,
         redirect: true,
         result: parsedStr
-      })
+      });
     }
   }
 
-  function handleError (err) {
-    console.error(err)
+  function handleError(err) {
+    console.error(err);
   }
 
   const handleChange = () => {
     setState({
-      ...state, 
-      text: document.getElementById("route-id").value
-    })
-  }
+      ...state,
+      text: document.getElementById('route-id').value
+    });
+  };
 
   return (
-    <div>
-      <Button 
-        children={'SWITCH'}
-        onClick={() => qrScanner()} 
-      />
-
-      <h1>Room Entry Page</h1>
+    <div className={'room_entry_page'}>
+      <header className={'room_header'}>
+        <h2 className={'room_header__title'}>Room Entry</h2>
+      </header>
       {!state.qrReaderStatus && (
+        <div className={'input_container'}>
+          <input
+            className={'input_container__input'}
+            id="route-id"
+            type="text"
+            value={state.text}
+            placeholder='Input room number'
+            onChange={() => handleChange()}
+          />
+
+          <ButtonRedirect route={`room/${state.text}`} text={'ENTER ROOM'} />
+        </div>
+      )}
+      
+      <div className={'entry_footer'}>
+        <button className={'entry_footer__switch'} onClick={() => qrScanner()}>QR READER</button>
+      </div>
+
+
+      {state.qrReaderStatus && (
         <div>
-          <input id="route-id" type="text" value={state.text} onChange={() => handleChange()}/>
-          
-          <ButtonRedirect 
-            route={`room/${state.text}`}
-            text={"ENTER ROOM"}
+          <QrReader
+            delay={300}
+            onError={handleError}
+            onScan={handleScan}
+            style={{ width: '100%' }}
           />
         </div>
       )}
-      {state.qrReaderStatus && (
-        <div>
-        <QrReader
-          delay={300}
-          onError={handleError}
-          onScan={handleScan}
-          style={{ width: '100%' }}
-        />
-        <p>{state.result}</p>
-      </div>
-      )}
-      {state.redirect && (
-        <Redirect to={`${state.result}`}/>
-      )}
+      {state.redirect && <Redirect to={`${state.result}`} />}
     </div>
   );
-}
- 
+};
+
 export default RoomEntryPage;

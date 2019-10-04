728318670ec97c9c6d15521080d5e7e3063f37da
Kevin Suen <kevinsuen11@gmail.com>
2019-10-04 16:11:58 -0400

    Add stylesheet for dashboard

--- src/pages/main/dashboard/dashboard.component.jsx
+++ src/pages/main/dashboard/dashboard.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import './dashboard.style.css';
+
 const DashboardPage = () => {
   return (
     <div>
       <h1>Dashboard Page</h1>
+
+      <h2>History</h2>
     </div>
   );
 }

6d71ed8b35d66af61539febf1c45eefeb248d03a
Kevin Suen <kevinsuen11@gmail.com>
2019-10-06 19:46:43 -0400

    Add global theme for material ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles'
+
 import { AuthProvider } from './firebase/auth.context';
 import PrivateRoute from './firebase/private-route.component';
 
@@ -13,24 +16,33 @@ import RoomPage from './pages/room/room.component';
 import SummaryPage from './pages/summary/summary.component';
 import './App.css';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#808edf',
+    }
+  },
+});
+
 function App() {
-  
   return (
     <AuthProvider>
       <Router>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/login" component={LoginPage} />
-          <Route exact path="/signup" component={SignupPage} />
-          <PrivateRoute exact path="/main" component={MainPage} />
-          <PrivateRoute
-            exact
-            path="/roominvitation/:id"
-            component={RoomInvitationPage}
-          />
-          <PrivateRoute exact path="/room/:id" component={RoomPage} />
-          <PrivateRoute exact path="/room/:id/summary" component={SummaryPage} />
-        </Switch>
+        <ThemeProvider theme={theme}>
+          <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route exact path="/login" component={LoginPage} />
+              <Route exact path="/signup" component={SignupPage} />
+              <PrivateRoute exact path="/main" component={MainPage} />
+              <PrivateRoute
+                exact
+                path="/roominvitation/:id"
+                component={RoomInvitationPage}
+              />
+              <PrivateRoute exact path="/room/:id" component={RoomPage} />
+              <PrivateRoute exact path="/room/:id/summary" component={SummaryPage} />
+          </Switch>
+        </ThemeProvider>
       </Router>
     </AuthProvider>
   );

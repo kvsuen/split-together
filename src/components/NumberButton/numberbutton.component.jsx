caa7bf1a54fa627ba9f8fa22299d7711b73517ae
jacksonf21 <jacksonf21@hotmail.com>
2019-10-08 17:12:47 -0400

    add room-entry number animation

--- src/components/NumberButton/numberbutton.component.jsx
+++ src/components/NumberButton/numberbutton.component.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import classnames from 'classnames'
+import './numberbutton.style.css'
 
-const NumberButton = ({ targetRoomCode, digit }) => {
+const NumberButton = ({ targetRoomCode, digit, select }) => {
+
+  const classnamesObj = {};
+  let testDigit = '';
+
+  if (select !== null && select !== undefined) {
+    testDigit = select.includes(digit); 
+  }
+
+  classnamesObj[`digit--clicked${digit}`] = testDigit;
+
+  const digitClass = classnames('digit', 
+    classnamesObj
+  )
 
   return (
-    <div className='digit' onClick={() => targetRoomCode(digit)}>
+    <div className={digitClass} onClick={() => targetRoomCode(digit)}>
       {digit}
     </div>
   );
